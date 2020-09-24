a13b5e0c5f62efb58886cd99d52ffdd76ef112cb
Ammar <ammar.n.sammour@gmail.com>
2020-09-24 19:01:21 +0300

    added lifecycle method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React, {useState, useEffect} from 'react';
 import db from './firebaseConfig'
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  const [header, setHeader] = useState('initial');
+const App = () => {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     header: "",
+  //     email: "",
+  //     fullname: "",   
+  //     users: []  
+  //   };
+  // }
+
+  // fetchData = async ()=>{
+  //   const res = await db.collection('demo').doc('header').get()
+  //   const data = res.data()
+  //   console.log(data);
+  //   this.setState({header: data.tilte})
+  //   const usersRes = await db.collection('users').get() 
+  //   console.log(usersRes);
+  //   const usersData = usersRes.docs.map(user => user.data())
+  //   console.log(usersData);
+  //   this.setState({users: usersData})
+  // }
+  // componentDidMount(){
+  //   setTimeout(() => {
+  //     this.fetchData()
+  //   }, 2000); 
+  // }
+  // componentDidUpdate  
+  // addUser = e => {
+  //     e.preventDefault()
+  //     db.collection('users').add({
+  //       fullName: this.state.fullname,
+  //       email: this.state.email,
+  //     })
+  //     this.setState({
+  //       email: "",
+  //       fullname:""
+  //     })
+  //   }
+
+  const [header, setHeader] = useState('');
   const [email, setEmail] = useState('');
   const [fullname, setFullname] = useState("");
   const [users, setUsers] = useState([])
@@ -33,10 +73,13 @@ function App() {
 
   useEffect(()=>{
     fetchData()
-  },[email])
+  },[email, header])
   
 
   
+    if (users === []){
+      return <h3>loading...</h3>
+    } else {
   return (
     <>
       <h1>{header}</h1>
@@ -58,9 +101,14 @@ function App() {
         <button type="submit">Submit</button>
       </form>
       <h4>users</h4>
-      {users.map(user => <div><span>Full Name: {user.fullName} </span><span>Email: {user.email} </span></div>)}
+      <table border="true">
+        <tbody>
+          <tr><th>Full Name</th><th>Email</th></tr>
+          {users.map((user, index) => <tr key={index}><td>{user.fullName}</td><td>{user.email} </td></tr>)}
+        </tbody>
+      </table>
       </>
       );
-  }
-
+    }
+}
 export default App;
